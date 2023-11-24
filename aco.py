5139f50b0e3efacef90ef2156e5cd017734d78d0
Kevin Nar <45304201+kpnaranj@users.noreply.github.com>
2023-11-24 05:45:09 -0500

    Updated aco algorithm to work with weights and occupancies. Tested costs

--- aco.py
+++ aco.py
@@ -26,12 +26,12 @@ class AntColony(object):
         self.campus_graph = campus_graph
         self.pheromone = np.ones(self.distances.shape) / len(distances)
         self.all_indexes = range(len(distances))
-        self.number_ants = 50
-        self.best_ants = 10
-        self.number_iterations = 1000
-        self.decay = 0.95
-        self.alpha = 1
-        self.beta = 1
+        self.number_ants = 30  # If we want fast converge we can reduce ants
+        self.best_ants = 5  # From the 20 paths received just see best 5
+        self.number_iterations = 700  # If we want fast converge we can reduce iterations
+        self.decay = 0.9  # Higher decay reduces pheromone path - range from [0.5 to 1] for evaporation
+        self.alpha = 0.7  # Higher alpha for more relevance to pheromones received
+        self.beta = 0.7  # Higher beta for more relevance to weight received
 
     def run(self):
         shortest_path = None
@@ -41,18 +41,21 @@ class AntColony(object):
         all_time_shortest_path = ("placeholder", np.inf)
         for _ in range(self.number_iterations):
             all_paths = self.generate_all_paths()
-
             self.spread_pheromone(all_paths, self.best_ants)
             shortest_path = min(all_paths, key=lambda x: x[1])
+            
             if shortest_path[1] < all_time_shortest_path[1]:
+                print("New path:{}".format(shortest_path))
+                print("This is new shortest:{}".format(shortest_path[1]))
                 all_time_shortest_path = shortest_path
+            
             self.pheromone = self.pheromone * self.decay
 
-        shortest_path = all_time_shortest_path[0]
-        best_path = self.edges_to_nodes_ordered(shortest_path)
-        weights = self.prepare_weights(best_path)
-        sum_weight = sum(weights)
-        return sum_weight, best_path, weights 
+        shortest_path, cost = all_time_shortest_path
+        path = self.edges_to_nodes_ordered(shortest_path)
+        weights = self.prepare_weights(path)
+        
+        return cost, path, weights
     # Spread pheromones from ants
 
     def spread_pheromone(self, all_paths, best_ants):
@@ -60,36 +63,58 @@ class AntColony(object):
         for path, path_distance in sorted_paths[:best_ants]:
             for move in path:
                 if path_distance != 0:
-                    self.pheromone[move] += 1.0 / self.distances[move]
+                    # Move path to the weights
+                    weights = self.campus_graph.edges[move]['weights']
+                    distance = sum(weights)
+                    self.pheromone[move] += 1.0 / distance
                 else:
                     self.pheromone[move] += 0
     # Generates the path distance available. Checks distances
 
     def generate_path_distance(self, path):
-        total_dist = 0
-        for item in path:
-            total_dist += self.distances[item]
-        return total_dist
+        # Format paths
+        formatted_path = self.edges_to_nodes_ordered(path)
+        weights = self.prepare_weights(formatted_path)
+        # Calculate the shortest distances
+        distance = sum(weights)
+        return distance
 
     def generate_all_paths(self):
         all_paths = []
         # example_node = [0,1,2,3,4]
         all_nodes = list(self.campus_graph.nodes)
-        for i in range(self.number_ants):
+        for _ in range(self.number_ants):
             # randomly select a starting node
             node_choice = np_choice(all_nodes)
             path = self.generate_path(node_choice)
+            # This will send the entire path and the shortest distance from that path
             all_paths.append((path, self.generate_path_distance(path)))
         return all_paths
-    # Generates a single path
+    
     def generate_path(self, start):
         path = []
         visited = set()
         visited.add(start)
         previous = start
-        for _ in range(len(self.distances) - 1):
-            move = self.pick_move(
-                self.pheromone[previous], self.distances[previous], visited)
+        for node in range(len(self.distances) - 1):
+            if(node == 0):
+                # Only for first node like (0,0), (1, 1)
+                distances = self.distances[previous]
+                ant_pheromone = self.pheromone[previous]
+            else:
+                # This is formatted as [1,2,3,4,5,...]
+                weights = self.campus_graph.edges[previous, start]["weights"]
+                occupancy = self.campus_graph.edges[previous, start]["occupancies"]
+                distances = 1 / weights
+                evaporation = 1 - self.decay
+                # update local with occupancy information to check availability of ants
+                # remove occupancy if not required. 
+                # Update pheromone locally
+                ant_pheromone = self.pheromone[previous] + (occupancy * evaporation)
+                
+            # We can do availability = 1/ occupancies
+            ant_pheromone = self.pheromone[previous]
+            move = self.pick_move(ant_pheromone, distances, visited)
             path.append((previous, move))
             previous = move
             visited.add(move)
@@ -112,7 +137,7 @@ class AntColony(object):
         pheromone[list(visited)] = 0
         visibility = self.generate_reciprocal(path_distance)
 
-        row = pheromone ** self.alpha * (visibility ** self.beta)
+        row = (pheromone ** self.alpha) * (visibility ** self.beta)
 
         norm_row = row / row.sum()
         # Randomly choose another node to move
@@ -152,20 +177,18 @@ class AntColony(object):
 
         return weights
 
-    # Example usage:
+    # Example usage: Create a file example.py and copy the following code, you will be able to test the code
     # -- code --
-    """ 
-    The following is an example of how to implement the algorithm in our code
-    from aco import AntColony
+    """ from aco import AntColony
     from createGraph import dist_map, create_campus_graph
+    import numpy as np
 
     campus_graph = create_campus_graph()
     # Sending format into array
     distances = np.array(dist_map)
     # It selects random nodes as any start, then it follows the shortest path and returns best result
     ant_colony = AntColony(distances, campus_graph)
-    shortest_path, weight, cost = ant_colony.run()
-    print("shorted_path: {}".format(shortest_path))
+    cost, path, weight = ant_colony.run()
+    print("shorted_path: {}".format(path))
     print("weights: {}".format(weight))
-    print("cost: {}".format(cost))
-    """
+    print("cost: {}".format(cost)) """
